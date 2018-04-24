ba5530ed29b35f568a296d4b5c09eb1651e90fbe
Mark Vonk <markjhvonk@gmail.com>
2018-04-24 21:22:09 +0200

    Rotating cube :D

--- dev/game.ts
+++ dev/game.ts
@@ -1,22 +1,46 @@
 import * as THREE from 'three';
+import { Mesh } from 'three';
 
 class Game {
+
+    public scene : THREE.Scene
+    public camera : THREE.PerspectiveCamera
+    public renderer : THREE.WebGLRenderer
+    public cube : THREE.Mesh
+
     constructor(){
         console.log("new game created!")
 
-        //Three.js stuff
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        // Make scene + camera
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-        var renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
+        // Create cube
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        this.cube = new THREE.Mesh( geometry, material );
+        this.scene.add( this.cube );
+
+        // Set camera position
+        this.camera.position.z = 5;
+
+        // Make renderer
+        this.renderer = new THREE.WebGLRenderer();
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( this.renderer.domElement );
 
         this.gameLoop()
     }
 
     gameLoop() : void {
-        console.log("loop test 1")
+        // console.log("loop test 1")
+
+        this.cube.rotation.x += 0.1;
+        this.cube.rotation.y += 0.1;
+
+        // Render scene
+	    this.renderer.render( this.scene, this.camera );
+
         requestAnimationFrame(() => this.gameLoop())
     }
 }
