a84fd3c608afd4367a46be897bcfc3f0890c841e
Mark Vonk <markjhvonk@gmail.com>
2018-04-24 20:11:44 +0200

    Initial push

---
+++ dev/game.ts
@@ -0,0 +1,24 @@
+import * as THREE from 'three';
+
+class Game {
+    constructor(){
+        console.log("new game created!")
+
+        //Three.js stuff
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+        var renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+
+        this.gameLoop()
+    }
+
+    gameLoop() : void {
+        console.log("loop test 1")
+        requestAnimationFrame(() => this.gameLoop())
+    }
+}
+
+window.addEventListener("load", () => new Game())
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './dev/game.ts',
+    output: {
+        path: __dirname + '/docs/js',
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    module: {
+        rules: [
+            { test: /\.ts?$/, loader: 'ts-loader' }
+        ]
+    }
+}
